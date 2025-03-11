b2dc3b24fdf8d7178cac6599166d240b1ff9a045
GILANG <wush3222@gmail.com>
2025-03-11 15:08:52 +0700

    add status filter and major filter

--- pages/CoursePage.py
+++ pages/CoursePage.py
@@ -11,7 +11,9 @@ class CoursePage:
         self.filter_button = (By.XPATH, '//*[@aria-label="Show/Hide filters"]')
         self.scrollbar_horizontal = (By.XPATH, '//*[@class="MuiTableContainer-root joy-13sfx72"]')
         self.status_filter = (By.XPATH, '//*[@class="MuiBox-root joy-1tzeee1"]')
+        self.major_filter = (By.XPATH, '//*[@aria-label="Filter by Jurusan"]')
         self.published_filter = (By.XPATH, '//*[@data-value="true"]')
+        self.draft_filter = (By.XPATH, '//*[@data-value="false"]')
 
     def click_filter_button(self):
         filter_button = WebDriverWait(self.driver, 10).until(
@@ -25,6 +27,12 @@ class CoursePage:
         )
         self.driver.execute_script("arguments[0].scrollLeft += 200;", scrollbar_horizontal)
 
+    def click_major_filter(self, major):
+        major_filter = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located(self.major_filter)
+        )
+        major_filter.click()
+        major_filter.send_keys(major)
 
     def click_status_filter(self):
         status_filter = WebDriverWait(self.driver, 10).until(
@@ -38,11 +46,35 @@ class CoursePage:
         )
         published_filter.click()
 
+    def click_draft_filter(self):
+        draft_filter = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located(self.draft_filter)
+        )
+        draft_filter.click()
+
+    def check_row_major(self):
+        all_rows = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_all_elements_located((By.XPATH, '//*[@aria-label="S1 Fakultas03 Jurusan01"]'))
+        )
+
+        for row in all_rows:
+            print(row.text)
+            assert row.text == 'S1 Fakultas03 Jurusan01'
+
     def check_row_is_published(self):
         all_rows = WebDriverWait(self.driver, 10).until(
             EC.presence_of_all_elements_located((By.XPATH, '//*[@class="MuiChip-label MuiChip-labelMd joy-tymi7a"]'))
         )
 
         for row in all_rows:
-            print(row)
+            print(row.text)
             assert row.text == 'Diterbitkan'
+
+    def check_row_is_draft(self):
+        all_rows = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_all_elements_located((By.XPATH, '//*[@class="MuiChip-label MuiChip-labelMd joy-tymi7a"]'))
+        )
+
+        for row in all_rows:
+            print(row.text)
+            assert row.text == 'Draft'
--- testcase/CourseTesting.py
+++ testcase/CourseTesting.py
@@ -21,12 +21,30 @@ class TestCoursePage:
         """Menutup WebDriver setelah setiap test."""
         self.driver.quit()
 
+    def test_filter_major(self):
+        time.sleep(3)
+        self.login_page.enter_username("uipm.admin_university.000001")
+        self.login_page.enter_password("uipm.01000001")
+        self.login_page.click_login_button()
+        time.sleep(7)  # Menunggu 3 detik setelah login
+        assert self.driver.current_url == "https://uipm.lms.vertical.id/dashboard"
+        assert "Halo Administrator Univ1!" == self.dashboard_page.account_name_dashboard_get_text()
+        self.dashboard_page.click_courses_page_sidebar_button()
+        time.sleep(3)
+        assert self.driver.current_url == "https://uipm.lms.vertical.id/courses"
+        time.sleep(3)
+        self.course_page.click_filter_button()
+        self.course_page.click_major_filter("S1 Fakultas03 Jurusan01")
+        time.sleep(3)
+        self.course_page.check_row_major()
+        time.sleep(3)
+
     def test_filter_status_published(self):
         time.sleep(3)
         self.login_page.enter_username("uipm.admin_university.000001")
         self.login_page.enter_password("uipm.01000001")
         self.login_page.click_login_button()
-        time.sleep(3)  # Menunggu 3 detik setelah login
+        time.sleep(7)  # Menunggu 3 detik setelah login
         assert self.driver.current_url == "https://uipm.lms.vertical.id/dashboard"
         assert "Halo Administrator Univ1!" == self.dashboard_page.account_name_dashboard_get_text()
         self.dashboard_page.click_courses_page_sidebar_button()
@@ -40,4 +58,25 @@ class TestCoursePage:
         self.course_page.click_status_filter()
         self.course_page.click_published_filter()
         self.course_page.check_row_is_published()
+        time.sleep(3)
+
+    def test_filter_status_draft(self):
+        time.sleep(3)
+        self.login_page.enter_username("uipm.admin_university.000001")
+        self.login_page.enter_password("uipm.01000001")
+        self.login_page.click_login_button()
+        time.sleep(7)  # Menunggu 3 detik setelah login
+        assert self.driver.current_url == "https://uipm.lms.vertical.id/dashboard"
+        assert "Halo Administrator Univ1!" == self.dashboard_page.account_name_dashboard_get_text()
+        self.dashboard_page.click_courses_page_sidebar_button()
+        time.sleep(3)
+        assert self.driver.current_url == "https://uipm.lms.vertical.id/courses"
+        time.sleep(3)
+        self.course_page.click_filter_button()
+        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        self.course_page.scroll_to_right()
+        self.driver.execute_script("window.scrollTo(0, 0);")
+        self.course_page.click_status_filter()
+        self.course_page.click_draft_filter()
+        self.course_page.check_row_is_draft()
         time.sleep(3)
